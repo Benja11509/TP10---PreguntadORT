c1c6882e833759c1afeb6e760decba60fd22a5cd
BENJAMIN LIBERMAN <49518992@ort>
2025-10-09 08:06:19 -0300

    primera subida

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tp10_preguntadort.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
